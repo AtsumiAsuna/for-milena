c6c91316c5eefeb4aad2dfc8df9f8b077f336f90
Valeria Yuldasheva <119105020+AtsumiAsuna@users.noreply.github.com>
2023-05-18 20:09:04 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Abs(x));
+            Console.WriteLine(Math.Max(x, y));
+            Console.WriteLine(Math.Sqrt(x));
+            Console.WriteLine(Math.Truncate(32.7865m));
+            //индивидуальное задание 26
+            double result;
+            result = Math.Sqrt(Math.Sqrt((((40 * 7 / 30) - (38 * 5 / 12)) / 1.9 + (7 / 8 - 7 / 30) * (1 * 9 / 11)) * 4.2) / 0.008) - (1 - Math.Sqrt(Math.Pow(1.12, 5)));
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //индивидуальное задание практическая 2 вариант 26
+            //задание 1
+            Console.WriteLine("Введите a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите p: ");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите g: ");
+            double g = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double Q;
+            //Высчитываем b
+            if (g > Math.Sin(a)) { b = g; }
+            else if (g == Math.Sin(a)) { b = a; }
+            else if (g < Math.Sin(a)) { b = p; }
+            //Высчитываем g
+            if ((-2 > a) && (a > 2)) { g = Math.Sin(a); }
+            else { g = Math.Cos(a); }
+            //Высчитываем Q
+            Q = ((a * a) + b) / (p + (g * g));
+            Console.WriteLine(Convert.ToString(Q));
+        }
+    }
+}
