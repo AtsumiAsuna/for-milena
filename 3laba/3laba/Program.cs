0c7c320bc43b5e1289bd144449885b13e4cd1d0b
Valeria Yuldasheva <119105020+AtsumiAsuna@users.noreply.github.com>
2023-05-25 18:29:30 +0300

    Add files via upload

---
+++ 3laba/3laba/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3laba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //практическая работа 3 вариант 28
+            //Юлдашева 24ИС
+
+            double a = 1.5;
+            int n0 = 2;
+            int nk = 8;
+            int deltaN = 2;
+            double x = 2.3;
+            double b = 0.7;
+
+            for (int n = n0; n <= nk; n += deltaN)
+            {
+                double xb = x * b;
+                double cosValue = Math.Cos(xn - b);
+                double denominator = Math.Pow(x, 2) - (b * Math.Sqrt(b));
+                double tgValue = Math.Tan(Math.Pow(n, 3));
+
+                double result = 0.5 * xb + (cosValue / denominator) + tgValue;
+
+                Console.WriteLine("n = " + n + ", Z = " + result);
+
+
+                //лабораторная работа 3 вариант 28
+                double sum = 0;
+                for (int k = 1; k <= 10; k++)
+                {
+                    double innerSum = 0;
+                    for (int n = 1; n <= k; n++)
+                    {
+                        innerSum += Math.Tan(10 * n);
+                    }
+                    sum += innerSum / k;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ 4laba/4aba/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4aba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //практическая работа 4 вариант 28
+            //Юлдашева 24ИС
+
+            int[,,] array = new int[3, 3, 3];
+            Random random = new Random();
+
+            //заполняем массив случайными числами
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        array[i, j, k] = random.Next(1, 10);
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    array[1, i, j] = i * j + 1; //формула для получения уникальных значений
+                }
+            }
+
+            //выводим массив на экран
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.Write(array[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+public class HelloWorld
+    {
+        static public void Main()
+        {
+            //лабораторная работа 4 вариант 28
+            //Юлдашева 24ИС
+
+            int[] array = { 2, 4, 6, 8, 10, 12, 14, 16 };
+            int n = array.Length;
+            int count = 0;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                if ((double)array[i] / array[n - i - 1] > 1)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количество элементов, удовлетворяющих условию: " + count);
+        }
+    }
+}
