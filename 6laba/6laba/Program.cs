070bb24c191aa61005bb752166d4b96d180138b2
Valeria Yuldasheva <119105020+AtsumiAsuna@users.noreply.github.com>
2023-05-25 18:45:43 +0300

    Add files via upload

---
+++ 6laba/6laba/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6laba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Практическая работа 6 вариант 28
+            //Юлдашева 24ИС-21
+
+            Console.Write("Введите количество чисел: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите точность: ");
+            double precision = double.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            StreamWriter sw = new StreamWriter("numbers.txt");
+
+            for (int i = 0; i < n; i++)
+            {
+                double num = rnd.NextDouble();
+                if (num < precision)
+                {
+                    num *= 1000;
+                }
+                sw.WriteLine(num);
+            }
+
+            sw.Close();
+            Console.WriteLine("Файл numbers.txt создан.");
+            Console.ReadKey();
+        }
+    }
+}
